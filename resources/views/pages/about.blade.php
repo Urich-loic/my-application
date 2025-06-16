3475d39e937fe8c595496dde0126fd0bddbfe3a4
Urich-loic <655808580ab@gmail.com>
2025-06-16 12:11:11 +0100

    make use of asset to load images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('title',"Hello city | ".config('app.name'))
 
 @section('content')
-<img src="/images/flag.jpeg" alt="">
+<img src="{{asset('/images/flag.jpeg')}}" alt="">
     <h1>Build with ❤️ by Engama</h1>
     <p>Revenir a la <a href="{{route('home')}}">home page</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', config('app.name'))
 
 @section('content')
-<img src="/images/flag.jpeg" alt="">
+<img src="{{asset('/images/flag.jpeg')}}" alt="">
     <h1>Hello from quebec</h1>
     <p>Its currently {{date('h:i:s A')}}</p>
 @endsection
