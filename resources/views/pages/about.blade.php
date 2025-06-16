4aabe0ef8ee4efbc69283729e164936efa18227b
Urich-loic <655808580ab@gmail.com>
2025-06-16 10:20:34 +0100

    Layout & section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About us | Hello city</title>
-</head>
-<body>
+@extends('base')
+@section('content')
     <h1>Build with ❤️ by Engama</h1>
     <p>Revenir a la <a href="/">home page</a></p>
-</body>
-</html>
+@endsection;
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello city</title>
-</head>
-<body>
+@extends('base')
+@section('content')
     <h1>Hello from quebec</h1>
     <p>Its currently {{date('h:i:s A')}}</p>
-    <footer>&copy; Copyright {{date('Y')}}  &middot; <a href="/about-us" >About</a></footer>
-</body>
-</html>
+@endsection;
