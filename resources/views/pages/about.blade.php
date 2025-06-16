8f35d1a0673c3bd06825cefbe12215f3a5ba5937
Urich-loic <655808580ab@gmail.com>
2025-06-16 10:29:31 +0100

    Titre mise a jour

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+
+@section('title','About us | Hello city')
+
 @section('content')
     <h1>Build with ❤️ by Engama</h1>
     <p>Revenir a la <a href="/">home page</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+
+@section('title', 'Hello city')
+
 @section('content')
     <h1>Hello from quebec</h1>
     <p>Its currently {{date('h:i:s A')}}</p>
