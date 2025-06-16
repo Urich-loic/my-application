bb21d268cebd27390b61fe8f44b3b3d882cdb407
Urich-loic <655808580ab@gmail.com>
2025-06-16 10:02:17 +0100

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>About us | Hello city</title>
 </head>
 <body>
-    <h1>Test</h1>
+    <h1>Build with ❤️ by Engama</h1>
+    <p>Revenir a la <a href="/">home page</a></p>
 </body>
 </html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello city</title>
+</head>
+<body>
+    <h1>Hello from quebec</h1>
+    <p>Its currently {{date('h:i:s A')}}</p>
+    <footer>&copy; Copyright {{date('Y')}}  &middot; <a href="/about-us" >About</a></footer>
+</body>
+</html>
